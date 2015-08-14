01d0cf4e2db2e02852491a13e86df2c0b3b5eca6
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2015-08-14 13:09:48 -0500

    Update and rename excale06-2005-cuest-doc.R to excale06-2005/excale06-2005-cuest-doc.R
    Cambio de carpeta.

--- excale06-2005/excale06-2005-cuest-doc.R
+++ excale06-2005/excale06-2005-cuest-doc.R
@@ -34,7 +34,7 @@ doc <- import("Excale06_2005_Docentes.sav")
 unlink(temp)
 rm(temp)
 
-#III. Escritura
+#3. Escritura
 #Escritura de archivo csv que permite una lectura más sencilla de la base de datos.
 #Procedimiento 1: Con librerías base
 #Escritura
