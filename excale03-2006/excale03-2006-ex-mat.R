cecdd4ee0102d310bc398d7b5068aa7d4f47d1a4
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2015-08-14 12:59:31 -0500

    Update and rename excale03-2006-ex-mat.R to excale03-2006/excale03-2006-ex-mat.R
    Cambio de carpeta.

--- excale03-2006/excale03-2006-ex-mat.R
+++ excale03-2006/excale03-2006-ex-mat.R
@@ -64,7 +64,7 @@ mat12[mat12 > 4] <- NA
 mat <- cbind(mat01, mat02, mat03, mat04, mat05, mat06, mat07, mat08, mat09, mat10, mat11, mat12, mat13)
 rm(mat01, mat02, mat03, mat04, mat05, mat06, mat07, mat08, mat09, mat10, mat11, mat12, mat13, anchos, nombres)
 
-#III. Escritura
+#3. Escritura
 #Escritura de archivo csv que permite una lectura más sencilla de la base de datos.
 #Procedimiento 1: Con librerías base
 #Escritura
