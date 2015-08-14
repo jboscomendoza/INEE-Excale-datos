3fc367dfcefa322f33ed8b0e30323a171538ee90
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2015-08-14 12:54:11 -0500

    Update and rename excale03-2006-ex-civ.R to excale03-2006/excale03-2006-ex-civ.R
    Cambio de carpeta.

--- excale03-2006/excale03-2006-ex-civ.R
+++ excale03-2006/excale03-2006-ex-civ.R
@@ -64,7 +64,7 @@ civ12[civ12 > 4] <- NA
 civ <- cbind(civ01, civ02, civ03, civ04, civ05, civ06, civ07, civ08, civ09, civ10, civ11, civ12, civ13)
 rm(civ01, civ02, civ03, civ04, civ05, civ06, civ07, civ08, civ09, civ10, civ11, civ12, civ13, anchos, nombres)
 
-#III. Escritura
+#3. Escritura
 #Escritura de archivo csv que permite una lectura más sencilla de la base de datos.
 #Procedimiento 1: Con librerías base
 #Escritura
